2e68d09467da9ea233f94ffa8c8b988bf4fb4246
Abhi <meabhishek1512@gmail.com>
2024-03-09 19:36:27 +0530

    mongo db setup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,8 @@
-export const signup = (req, res) => {
-    res.send("signupUser")
+export const signup = async (req, res) => {
+    try {
+        const { fullName, username, password, confirmPassword, gender } =
+            req.body
+    } catch (error) {}
 }
 
 export const login = (req, res) => {
