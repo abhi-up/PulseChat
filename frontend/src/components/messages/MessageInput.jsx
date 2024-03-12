8a0de1ba10e882b6d83fcea4a2b87f23a11c9230
Abhi <meabhishek1512@gmail.com>
2024-03-12 19:05:42 +0530

    get messages functionality endpoint

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -5,6 +5,7 @@ import useSendMessage from "../../hooks/useSendMessage"
 const MessageInput = () => {
     const [message, setMessage] = useState("")
     const { loading, sendMessage } = useSendMessage()
+
     const handleSubmit = async (e) => {
         e.preventDefault()
         if (!message) return
